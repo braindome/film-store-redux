01e01a28455ae3b0a388bfa01dbb0d480875bdcd
Antonio Saracino <antonio.saracino83@gmail.com>
2023-10-13 13:42:24 +0200

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "film-store-redux",
+  base: "redux-test",
   plugins: [react()],
 })
